437ec1a11f67f729d5ccf02fd66c7fff47adc752
arfaaliya <147474459+arfaaliya@users.noreply.github.com>
2023-10-09 22:34:11 -0700

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("addition :"+(a+b));
+		
+		
+		
+	
+	
+	
+	
+	}
+
+}
+
